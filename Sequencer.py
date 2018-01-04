d4638fc7b2dbe2c494dd5969bc7c845cf6c4a7e8
Nicolas Klenert <nicolas_klenert@web.de>
2018-01-04 01:39:21 +0100

    added DoubleDescriptionMethod (without a test for extremal rays)

--- Sequencer.py
+++ Sequencer.py
@@ -38,6 +38,50 @@ class Sequencer(object):
         #I should be the minimal indexlist with full rank
         return I
 
+    # def _calculateTensor(A,s, repeatFirstScalar = True):
+    #     Am,An = A.shape
+    #     sn = len(s)
+    #     if repeatFirstScalar:
+    #         A = A.reshape((1,Am,An)) #add third dimension
+    #         s = s.reshape((sn,1,1)) #point vector to third axis (in python axis=0)
+    #         #we reduce the third axis of A (so axis=0) and the first axis of s (axis = 2)
+    #         axis = ([2],[0])
+    #     else: #makes no difference!
+    #         A = A.reshape((Am,1,An))
+    #         s= s.reshape((1,sn,1))
+    #         #just take the index of a 1
+    #         axis = ([2],[1])
+    #     #now multiply so we get an 3dim tensor
+    #     return np.tensordot(s,A,axes=axis)
+    #     #for repeating of tensors: look at np.tile and np.repeat
+    #     ALL unnecessary! Tensordot automatacly creates new dimensions if needed
+
     def doubleDescriptionMethod(A, tol = 0.01):
+        m, n = A.shape
         #get a fullrank indexlist
         I = Sequencer._getMinimunIndexList(A,tol)
+        #important: V is different here then in the skript! it is transposed
+        #therefore V is a Vector of the Vi
+        V = np.negative(np.linalg.inv(A[I,:])).T
+        J = set(range(n)).difference_update(I)
+        while len(J) != 0:
+            j = J.pop()
+            a = A[j,:]  #j-th row
+            #calcualte scalarproduct of a and vi (vi the columns of V)
+            scalars = np.dot(V,a)
+            #TODO: delete all unnecessarty Vi
+            V1 = V[scalars <= 0] #all vi with si <= 0
+            #create V2: all si*vj-sj*vi with si > 0, sj < 0
+            Vneg = V[scalars < 0]
+            Vpos = V[scalars > 0]
+            scalars_neg = scalars[scalars < 0]
+            scalars_pos = scalars[scalars > 0]
+            #calculate rightSummand (sj*vi)
+            #at the end we want to slice the tensor (on the third axis; axis=0)
+            rightSummand = np.tensordot(Vpos,scalar_neg, axes=0).reshape((-1,v_n),order='F')
+            #calculate leftSummand (si*vj)
+            leftSummand = np.tensordot(Vneg, scalar_pos, axes= 0).reshape((-1,v_n),order='C')
+            V2 = leftSummand - rightSummand
+            #TODO: delete all unnecessarty Vi
+            V = numpy.concatenate((V1,V2))
+        return V
